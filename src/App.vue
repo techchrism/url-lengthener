2d46e9f0c0ca09e4bb66bbdbab0e1e8026c5cc31
TheTechdoodle <admin+github@darkender.com>
2020-07-01 20:23:22 -0700

    revise title bar and remove unused component

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,29 @@
 <template>
     <v-app>
-        <v-app-bar
-                app
-                color="primary"
-                dark
-        >
-            <div class="d-flex align-center">
-                <v-img
-                        alt="Vuetify Logo"
-                        class="shrink mr-2"
-                        contain
-                        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-                        transition="scale-transition"
-                        width="40"
-                />
-
-                <v-img
-                        alt="Vuetify Name"
-                        class="shrink mt-1 hidden-sm-and-down"
-                        contain
-                        min-width="100"
-                        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-                        width="100"
-                />
-            </div>
-
+        <v-app-bar app color="primary" dark>
+            <v-toolbar-title class="headline">
+                <span class="font-weight-light">Url Lengthener</span>
+                <v-btn icon
+                       href="https://github.com/TheTechdoodle/url-lengthener"
+                >
+                    <v-icon>mdi-github</v-icon>
+                </v-btn>
+            </v-toolbar-title>
             <v-spacer></v-spacer>
-
-            <v-btn
-                    href="https://github.com/vuetifyjs/vuetify/releases/latest"
-                    target="_blank"
-                    text
-            >
-                <span class="mr-2">Latest Release</span>
-                <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
+            <dark-switch/>
         </v-app-bar>
 
-        <v-content>
-            <HelloWorld/>
-        </v-content>
+        <v-main>
+            Hello
+        </v-main>
     </v-app>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld';
+    import DarkSwitch from './components/DarkSwitch';
 
     export default {
         name: 'App',
-
-        components: {
-            HelloWorld
-        },
-
-        data: () => ({
-            //
-        })
+        components: {DarkSwitch}
     };
 </script>
