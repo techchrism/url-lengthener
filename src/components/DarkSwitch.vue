91e9da5e74dc40dd170f09eeefda43421bd32552
TheTechdoodle <admin+github@darkender.com>
2020-07-01 20:19:48 -0700

    add dark mode switch

---
+++ src/components/DarkSwitch.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-switch color="accent" v-model="$vuetify.theme.dark" :append-icon="icon" hide-details label="Dark Mode"/>
+</template>
+
+<script>
+    import localforage from 'localforage';
+
+    export default {
+        name: 'DarkSwitch',
+        computed: {
+            icon()
+            {
+                return 'mdi-' + (this.$vuetify.theme.dark ? 'weather-night' : 'white-balance-sunny');
+            }
+        },
+        watch: {
+            '$vuetify.theme.dark': function(dark)
+            {
+                localforage.setItem('theme', dark ? 'dark' : 'light');
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
 import Vue from 'vue';
 import App from './App.vue';
-import vuetify from './plugins/vuetify';
+import Vuetify from 'vuetify/lib';
+import localforage from 'localforage';
+
+localforage.config({
+    name: 'url-lengthener'
+});
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
-new Vue({
-    vuetify,
-
-    render: function(h)
-    {
-        return h(App);
-    }
-}).$mount('#app');
+localforage.getItem('theme').then(theme =>
+{
+    new Vue({
+        vuetify: new Vuetify({
+            theme: {
+                dark: theme !== 'light'
+            }
+        }),
+        render: function(h)
+        {
+            return h(App);
+        }
+    }).$mount('#app');
+});
