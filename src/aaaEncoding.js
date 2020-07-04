08266eae9cc17259749dc1a81dc09ae858580868
Techdoodle <26680599+TheTechdoodle@users.noreply.github.com>
2020-07-04 09:12:04 -0700

    remove debugging info and reduce "replace" calls

--- src/aaaEncoding.js
+++ src/aaaEncoding.js
@@ -1,21 +1,16 @@
 export function aaaEncode(str)
 {
-    return str.split('').map(char =>
-    {
-        return char.charCodeAt(0).toString(2).padStart(8, '0').replace(/0/g, 'a')
-            .replace(/1/g, 'A');
-    }).join('');
+    return str.split('').map(char => char.charCodeAt(0).toString(2).padStart(8, '0'))
+        .join('').replace(/0/g, 'a').replace(/1/g, 'A');
 }
 
 export function aaaDecode(str)
 {
     let data = '';
-    console.log('Decoding');
-    console.log(str);
+    let binaryStr = str.replace(/a/g, '0').replace(/A/g, '1');
     for(let i = 0; i < str.length / 8; i++)
     {
-        data += String.fromCharCode(parseInt(str.substring(i * 8, (i * 8) + 8).replace(/a/g, '0')
-                    .replace(/A/g, '1'), 2));
+        data += String.fromCharCode(parseInt(binaryStr.substring(i * 8, (i * 8) + 8), 2));
     }
     return data;
 }
